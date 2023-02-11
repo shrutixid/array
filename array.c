8087035c7e1eb324cbbe342d6abb1cb8a3bb6110
shrutixid <123883088+shrutixid@users.noreply.github.com>
2023-02-11 20:04:13 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+ printf("The following program is to create an linear array of size =5\n\n\n");
+ int arr[5]={1,2,3,4,5};
+ printf("the array is :-\n");
+ for(int i=0;i<5;i++)
+ {
+  printf("%d\t",arr[i]);
+ }
+}
